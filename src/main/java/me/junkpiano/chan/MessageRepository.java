9e5ab3656b2603d511ce422b8d81e1193fa3e5a7
Yusuke Ohashi <github@junkpiano.me>
2018-10-25 01:00:17 +0900

    first service

---
+++ src/main/java/me/junkpiano/chan/MessageRepository.java
@@ -0,0 +1,12 @@
+package me.junkpiano.chan;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MessageRepository extends CrudRepository<Message, Long> {
+    List<Message> findByOrderByIdDesc(Pageable pageable);
+}
