34810e0e53207bd4be2f19a9017ea7606ee3e2b3
Yusuke Ohashi <github@junkpiano.me>
2018-10-26 18:54:44 +0900

    WIP: adding authentication

---
+++ src/main/java/me/junkpiano/chan/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package me.junkpiano.chan;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.
+                authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+    }
+}
